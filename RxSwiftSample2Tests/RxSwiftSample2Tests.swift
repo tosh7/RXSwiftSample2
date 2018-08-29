06383c9e9afd77964ac5403fc05cbcbc70ec9476
tosh7 <zlia.6.lj.425@gmail.com>
2018-08-29 17:56:54 +0900

    Initial Commit

---
+++ RxSwiftSample2Tests/RxSwiftSample2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RxSwiftSample2Tests.swift
+//  RxSwiftSample2Tests
+//
+//  Created by Satoshi Komatsu on 2018/08/29.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwiftSample2
+
+class RxSwiftSample2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
