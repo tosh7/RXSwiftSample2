7ff49922a4bb4bb87f64408d665b1368e6c6cd18
tosh7 <zlia.6.lj.425@gmail.com>
2018-08-30 16:41:16 +0900

    subscribe

---
+++ RxSwiftSample2/Sources/MainViewController/FirstTestViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  RxSwiftSample2
+//
+//  Created by Satoshi Komatsu on 2018/08/29.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    
+    let dissposeBag = DisposeBag()
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //コンソールに出力する場合は$0を使用する
+//        textField.rx.text
+//            .subscribe(onNext: {[weak self] value in
+//                print($0!)
+//        })
+//        .disposed(by: dissposeBag)
+        
+        //subscribeで書くとこっち
+        textField.rx.text
+            .subscribe(onNext: {[weak self] value in
+                self?.label.text = value!
+            })
+            .disposed(by: dissposeBag)
+        
+        //bindToで書くとこっち
+    }
+
+}
+
---
+++ RxSwiftSample2/Sources/StartViewController/StartViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartViewController.swift
+//  RxSwiftSample2
+//
+//  Created by Satoshi Komatsu on 2018/08/30.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+    
+}
